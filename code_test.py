a4cb66f8f3bf3c0d3c1c13f62bf8228ef86f5d19
J-Okoto <okotojr@gmail.com>
2020-09-02 20:14:19 +0300

    added test case to check if object is instantiated correctly

--- code_test.py
+++ code_test.py
@@ -10,3 +10,10 @@ class TestClass(unittest.TestCase):
         Method that runs before each individual test methods run.
         """
         self.new_user = User('J-Okoto','hushpuppy')
+
+    def test_init(self):
+        """
+        test case to chek if the object has been initialized correctly
+        """
+        self.assertEqual(self.new_user.username,'J-Okoto')
+        self.assertEqual(self.new_user.password,'hushpuppy01')   
