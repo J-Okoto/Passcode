2c24ee0c7833b8f720ec6a3b1a1e12fb30f1d227
J-Okoto <okotojr@gmail.com>
2020-09-02 20:10:37 +0300

    added test class in code_test.py file

--- code_test.py
+++ code_test.py
@@ -1,2 +1,12 @@
 import unittest
 from code import User
+
+class TestClass(unittest.TestCase):
+    """
+    A Test class that defines test cases for the User class.
+    """
+    def setUp(self):
+        """
+        Method that runs before each individual test methods run.
+        """
+        self.new_user = User('J-Okoto','hushpuppy')
