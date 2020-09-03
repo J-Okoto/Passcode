1d139e5f4fa0047cf68209df84fc1bf45b94b3b4
J-Okoto <okotojr@gmail.com>
2020-09-03 16:23:23 +0300

    added method to define user credentials to be stored

--- code.py
+++ code.py
@@ -22,4 +22,11 @@ class Credentials():
     Create credentials class to help create new objects of credentials
     """
     credentials_list = []
+    def __init__(self,account,userName, password):
+        """
+        method that defines user credentials to be stored
+        """
+        self.account = account
+        self.userName = userName
+        self.password = password
     
