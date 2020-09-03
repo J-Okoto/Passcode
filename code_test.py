c1921d29fe7fd5a00df00124a225904ada0feb85
J-Okoto <okotojr@gmail.com>
2020-09-03 19:12:26 +0300

    test 5 failed

--- code_test.py
+++ code_test.py
@@ -69,7 +69,7 @@ class TestCredentials(unittest.TestCase):
         test method to test if we can remove an account credentials from our credentials_list
         """
         self.new_credential.save_details()
-        test_credential = Credentials("Twitter","mikeycharles","Mfh45hfk")
+        test_credential = Credentials("Twitter","J-Okoto","hushpuppy03")
         test_credential.save_details()
 
         self.new_credential.delete_credentials()
