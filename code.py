1e0b5d4cbd4401e5cfc740dfc41438fe8e0053b9
J-Okoto <okotojr@gmail.com>
2020-09-03 07:50:52 +0300

    added code in code.py file to qualify second test case which failed earlier.The second test passed

--- code.py
+++ code.py
@@ -3,10 +3,17 @@ class User:
     Class that generates new instances of a user
     """
 
-    pass
+    user_list = []
     def __init__(self, username, password):
         """
         __init__method that defines the properties of a user.
         """
         self.username = username
         self.password = password
+
+    def save_user(self):
+        """
+        A method that saves a new user instace into the user list
+        """
+        User.user_list.append(self)
+    
